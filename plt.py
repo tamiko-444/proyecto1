449f5d1cc0b57c18adc1c9a6d5392d10e27529c4
Jorge Lasso <jorge.lasso@estudiante.uamerica.edu.co>
2024-07-16 10:16:28 -0500

    Agregue para par o impar y otro para graficar

---
+++ plt.py
@@ -0,0 +1,11 @@
+#Graficar 
+import matplotlib.pyplot as plt
+import numpy as np 
+
+x = np.linspace(-5,5,100)
+y = x**3 
+plt.plot(x,y)
+plt.title("funcion en python")
+
+plt.grid()
+plt.show()
---
+++ prueba2.py
@@ -0,0 +1,7 @@
+# Si el num es par o impar
+num = int(input("Ingrese un numero mayor a cero: "))
+
+if num%2 == 0:
+    print(f"{num} es un numero par")
+else:
+    print(f"{num} es un numero impar")

d8f471afbacbca66ce937da432754189c0257d49
Jorge Lasso <jorge.lasso@estudiante.uamerica.edu.co>
2024-07-16 10:49:40 -0500

    a

--- plt.py
+++ plt.py
@@ -4,8 +4,8 @@ import numpy as np
 
 x = np.linspace(-5,5,100)
 y = x**3 
-plt.plot(x,y)
-plt.title("funcion en python")
-
+plt.plot(x,y, "g.-", label = "x3", linewidth = 6)
+plt.title("funcion cubica")
+plt.legend
 plt.grid()
 plt.show()

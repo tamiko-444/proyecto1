e2fd530afdda24973177dc13128c86c0611d36fb
tamiko-444 <valeria.rojas_ji@estudiantes.uamerica.edu.co>
2024-07-16 11:35:53 -0500

    modifique plt.py

--- plt.py
+++ plt.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 
-x = np.linspace(-5,5,100)
+x = np.linspace(-4,5,100)
 y = x**3 
 plt.plot(x,y,"r.-", label = "x3", linewidth = 3)
 plt.title("funcion cubica")
